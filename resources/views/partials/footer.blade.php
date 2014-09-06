c5031702974e5c830de9811cf4fa92f8bfed9040
Nicolas Widart <n.widart@gmail.com>
2014-09-06 15:22:37 +0200

    Adding the about page link in the footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,7 +6,7 @@
                     <li class="pull-right"><a href="#top">Back to top</a></li>
                     <li>&copy; {{ date('Y') }} Nicolas Widart</li>
                     <li class="{{ Request::is('/') ? 'active' : ''}}"><a href="{{ URL::to('/') }}">Home</a></li>
-                    <li class="{{ Request::is('about') ? 'active' : ''}}"><a href="">About</a></li>
+                    <li class="{{ Request::is('about') ? 'active' : ''}}"><a href="{{ URL::route('about') }}">About</a></li>
                     <li class="{{ Request::is('blog*') ? 'active' : ''}}"><a href="{{ URL::route('blog.index'); }}">Blog</a></li>
                     <li class="{{ Request::is('project') ? 'active' : ''}}"><a href="">Projects</a></li>
                 </ul>
