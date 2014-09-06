6256388ad12557869f73a5c811cd1cb399dde7cd
Nicolas Widart <n.widart@gmail.com>
2014-09-06 15:45:49 +0200

    Adding the about link on homepage

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -6,7 +6,7 @@
       <img src="//www.gravatar.com/avatar/{{ md5('n.widart@gmail.com') }}?size=150" alt="" class="rounded pull-right pull-top"/>
       <h1>Hi,</h1>
       <p>My name is Nicolas Widart, and I'm a <strong>web developper</strong>. I love the <a href="www.laravel.com">Laravel</a> framework and new technologies.</p>
-      <p><a class="btn btn-primary btn-lg">Learn more about me</a></p>
+      <p><a class="btn btn-primary btn-lg" href="{{ URL::route('about') }}">Learn more about me</a></p>
     </div>
 </div>
 <div class="row">
