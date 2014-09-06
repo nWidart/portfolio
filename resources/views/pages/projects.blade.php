f7a3a8534132b9641997c460bbf23cf3a5cff716
Nicolas Widart <n.widart@gmail.com>
2014-09-06 15:56:47 +0200

    Cleaning up the projects page

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -8,16 +8,25 @@ Projects | @parent
 <h1>Projects</h1>
 
 <h2>Main projects</h2>
-<ul>
-    <li><a href="https://github.com/nWidart/DbExporter">DbExporter</a>
-    A Laravel package to export your database structure as a migration file.</li>
-    <li><a href="https://github.com/nWidart/HttpStatus-l4-package">HttpStatus</a>
-    A package to quickly add error pages (403, 404, 500 and 503) to your website.</li>
-    <li><a href="https://github.com/nWidart/projectLister">Project Lister</a>
-    A package to easily list your projects from Github, Bitbucket or Sourceforge.</li>
-    <li><a href="https://github.com/nWidart/Modules">Modules</a>
-    A package to quickily create the structure for modules in Laravel.</li>
-    <li><a href="https://github.com/nWidart/Laravel-forecast">Laravel-forecast</a>
-    Laravel-forecast provides a service provider and a facade arround the Forecast-php wrapper.</li>
+<ul class="bullet">
+    <li>
+        <a href="https://github.com/nWidart/DbExporter" target="_blank">DbExporter</a><br/>
+        A Laravel package to export your database structure as a migration file.
+    </li>
+    <li>
+        <a href="https://github.com/nWidart/HttpStatus-l4-package" target="_blank">HttpStatus</a><br/>
+        A package to quickly add error pages (403, 404, 500 and 503) to your website.
+    </li>
+    <li>
+        <a href="https://github.com/nWidart/projectLister" target="_blank">Project Lister</a><br/>
+        A package to easily list your projects from Github, Bitbucket or Sourceforge.
+    </li>
+    <li>
+        <a href="https://github.com/nWidart/Modules" target="_blank">Modules</a><br/>
+        A package to quickily create the structure for modules in Laravel.</li>
+    <li>
+        <a href="https://github.com/nWidart/Laravel-forecast" target="_blank">Laravel-forecast</a><br/>
+        Laravel-forecast provides a service provider and a facade arround the Forecast-php wrapper.
+    </li>
 </ul>
 @stop
