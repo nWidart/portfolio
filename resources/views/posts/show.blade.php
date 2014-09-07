fec3b566a8b99206c85a3a5f1fc3fe955bace664
Nicolas Widart <n.widart@gmail.com>
2014-09-07 09:04:44 +0200

    Adding date to post page. Adding back to post list.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,15 @@
 @stop
 
 @section('content')
-<h1>{{ $post->title }}</h1>
+<div class="row">
+    <div class="col-lg-12">
+        <span class="linkBack">
+            <a href="{{ URL::route('blog.index') }}"><i class="glyphicon glyphicon-chevron-left"></i> Back to post list</a>
+        </span>
+        <h1>{{ $post->title }}</h1>
+        <span class="date">Posted on {{ $post->date }}</span>
 
-{{ $post->content }}
+        {{ $post->content }}
+    </div>
+</div>
 @stop
