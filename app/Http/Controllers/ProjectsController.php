1307203b0a0a84d4a0f09f0ffb4c90942e6a8396
Nicolas Widart <n.widart@gmail.com>
2014-09-09 21:55:49 +0200

    Caching the github repositories

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -1,17 +1,21 @@
 <?php namespace Nwidart\Http\Controllers;
 
-use Github\Client;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Routing\Controller;
 use GrahamCampbell\GitHub\Facades\GitHub;
+use Illuminate\Support\Facades\Cache;
 
 class ProjectsController extends Controller
 {
     public function index()
     {
-        $repositories = GitHub::api('user')->repositories('nwidart', 'owner', 'pushed', 'desc');
-        $repositories = new Collection($repositories);
-        $repositories = $repositories->take(5);
+        if (!Cache::has('github-projects')) {
+            $repositories = GitHub::api('user')->repositories('nwidart', 'owner', 'pushed', 'desc');
+            $repositories = new Collection($repositories);
+            $repositories = $repositories->take(5);
+            Cache::put('github-projects', $repositories, 60);
+        }
+        $repositories = Cache::get('github-projects', []);
 
         return view('pages.projects', compact('repositories'));
     }
--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -5,45 +5,46 @@ Projects | @parent
 @stop
 
 @section('content')
-<h1>Projects</h1>
-
-<h2>Main projects</h2>
-<ul class="bullet">
-    <li>
-        <a href="https://github.com/nWidart/DbExporter" target="_blank">DbExporter</a><br/>
-        A Laravel package to export your database structure as a migration file.
-    </li>
-    <li>
-        <a href="https://github.com/nWidart/HttpStatus-l4-package" target="_blank">HttpStatus</a><br/>
-        A package to quickly add error pages (403, 404, 500 and 503) to your website.
-    </li>
-    <li>
-        <a href="https://github.com/nWidart/projectLister" target="_blank">Project Lister</a><br/>
-        A package to easily list your projects from Github, Bitbucket or Sourceforge.
-    </li>
-    <li>
-        <a href="https://github.com/nWidart/Modules" target="_blank">Modules</a><br/>
-        A package to quickily create the structure for modules in Laravel.</li>
-    <li>
-        <a href="https://github.com/nWidart/Laravel-forecast" target="_blank">Laravel-forecast</a><br/>
-        Laravel-forecast provides a service provider and a facade arround the Forecast-php wrapper.
-    </li>
-</ul>
-
-<h2>Side projects websites</h2>
-
-<ul class="bullet">
-    <li>
-        <a href="http://forum.tplpc.com">Tout Pour Le PC</a>
-    </li>
-</ul>
-
-<a href="https://github.com/nWidart" target="_blank" class="btn btn-primary"><i class="glyphicon glyphicon-share-alt"></i> View all my projects on GitHub</a>
-
-<h2>Latest Activity</h2>
-<ul>
-    <?php foreach($repositories as $repository): ?>
-        <li>{{ $repository['name'] }}</li>
-    <?php endforeach; ?>
-</ul>
+<div class="row">
+    <div class="col-md-8">
+        <h1>Projects</h1>
+        <h2>Main projects</h2>
+        <ul class="bullet">
+            <li>
+                <a href="https://github.com/nWidart/DbExporter" target="_blank">DbExporter</a><br/>
+                A Laravel package to export your database structure as a migration file.
+            </li>
+            <li>
+                <a href="https://github.com/nWidart/HttpStatus-l4-package" target="_blank">HttpStatus</a><br/>
+                A package to quickly add error pages (403, 404, 500 and 503) to your website.
+            </li>
+            <li>
+                <a href="https://github.com/nWidart/projectLister" target="_blank">Project Lister</a><br/>
+                A package to easily list your projects from Github, Bitbucket or Sourceforge.
+            </li>
+            <li>
+                <a href="https://github.com/nWidart/Modules" target="_blank">Modules</a><br/>
+                A package to quickily create the structure for modules in Laravel.</li>
+            <li>
+                <a href="https://github.com/nWidart/Laravel-forecast" target="_blank">Laravel-forecast</a><br/>
+                Laravel-forecast provides a service provider and a facade arround the Forecast-php wrapper.
+            </li>
+        </ul>
+        <h2>Side projects websites</h2>
+        <ul class="bullet">
+            <li>
+                <a href="http://forum.tplpc.com">Tout Pour Le PC</a>
+            </li>
+        </ul>
+        <a href="https://github.com/nWidart" target="_blank" class="btn btn-primary"><i class="glyphicon glyphicon-share-alt"></i> View all my projects on GitHub</a>
+    </div>
+    <div class="col-md-3">
+        <h2>Latest Activity</h2>
+        <ul class="bullet">
+            <?php foreach($repositories as $repository): ?>
+                <li><a href="{{ $repository['html_url'] }}" target="_blank">{{ $repository['name'] }}</a></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+</div>
 @stop
