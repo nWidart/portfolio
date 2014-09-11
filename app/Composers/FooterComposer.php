bba88e3d9bbaa1c13e98100d3da7fa3becbbd3f1
Nicolas Widart <n.widart@gmail.com>
2014-09-11 14:06:51 +0200

    Desactivating the twitter feed temporarely/

--- app/Composers/FooterComposer.php
+++ app/Composers/FooterComposer.php
@@ -23,8 +23,8 @@ class FooterComposer
     {
         $activities = $this->githubActivity->forUser('nwidart')->activities(3);
 
-        $twitterPosts = $this->twitter->posts('nicolaswidart', 3);
-
+        //$twitterPosts = $this->twitter->posts('nicolaswidart', 3);
+        $twitterPosts = [];
         $view->with(compact('activities', 'twitterPosts'));
     }
 }
