508d336378c2145a9ebc3493ff7783f7504546f1
Nicolas Widart <n.widart@gmail.com>
2014-09-12 13:57:56 +0200

    not escaping the html

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,20 +5,20 @@
     <div class="col-md-4 col-md-offset-3">
         <h1>Login</h1>
         @include('partials.errors')
-        {{ Form::open() }}
+        {!! Form::open() !!}
         	<div class='form-group'>
-        	    {{ Form::label('email', 'Email:') }}
-        	    {{ Form::email('email', Input::old('email'), ['class' => 'form-control', 'placeholder' => 'Email']) }}
+        	    {!! Form::label('email', 'Email:') !!}
+        	    {!! Form::email('email', Input::old('email'), ['class' => 'form-control', 'placeholder' => 'Email']) !!}
         	</div>
         	<div class='form-group'>
-        	    {{ Form::label('password', 'Password:') }}
-        	    {{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) }}
+        	    {!! Form::label('password', 'Password:') !!}
+        	    {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}
         	</div>
 
         	<div class='form-group'>
-        	    {{ Form::submit('Login', ['class' => 'btn btn-primary form-control']) }}
+        	    {!! Form::submit('Login', ['class' => 'btn btn-primary form-control']) !!}
         	</div>
-        {{ Form::close() }}
+        {!! Form::close() !!}
     </div>
 </div>
 
