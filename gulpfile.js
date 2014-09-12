b80c3c700527160bc1a6ba9ed1cddea55a329d5c
Nicolas Widart <n.widart@gmail.com>
2014-09-12 16:51:23 +0200

    Adding gulp for assets.

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+'use strict';
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    notify = require('gulp-notify'),
+    path = require('path'),
+    minifyCSS = require('gulp-minify-css'),
+    rename = require("gulp-rename"),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+// Less handling
+var less_path = './public/assets/less/**/*.less';
+gulp.task('less', function () {
+    gulp.src('./public/assets/less/bootswatch.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./public/assets/css'))
+        .pipe(notify("Less compiled"))
+        .pipe(minifyCSS({keepBreaks:false}))
+        .pipe(rename(function (path) {
+            path.basename += ".min";
+            path.extname = ".css"
+        }))
+        .pipe(gulp.dest('./public/assets/css/dist/'))
+        .pipe(notify("Css Minified!"));
+});
+
+// Minify and copy all JavaScript (except vendor scripts)
+var js_path = './public/assets/js/**/*.js';
+gulp.task('scripts', function() {
+    return gulp.src('./public/assets/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/assets/js/dist'))
+        .pipe(notify("Js compiled"))
+        .pipe(concat('all.min.js'))
+        .pipe(gulp.dest('./public/assets/js/dist'))
+        .pipe(notify("Js minified"));
+});
+
+// File watchers
+gulp.task('watch-less', function() {
+    gulp.watch(less_path, ['less']);
+});
+gulp.task('watch-js', function() {
+    gulp.watch(js_path, ['scripts']);
+});
+
+gulp.task('default', ['watch-less', 'watch-js']);
