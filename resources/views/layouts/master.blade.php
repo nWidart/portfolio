f4cd755848e0c3de917668577117950194417af5
Nicolas Widart <n.widart@gmail.com>
2014-09-12 17:06:07 +0200

    Adding environment checks for correct asset inclusion.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,9 +11,15 @@
     @show
    </title>
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" media="screen">
-    <link rel="stylesheet" href="{{ asset('/assets/css/bootswatch.css') }}">
     <link rel="stylesheet" href="{{ asset('/assets/css/prism.css') }}">
+    <?php if (App::environment() == 'development'): ?>
+        <link rel="stylesheet" href="{{ asset('/assets/css/bootswatch.css') }}">
+    <?php else: ?>
+        <link rel="stylesheet" href="{{ asset('/assets/css/dist/bootswatch.min.css') }}">
+    <?php endif; ?>
+
 </head>
 <body>
 
@@ -24,10 +30,13 @@
 </div>
 @include('partials.footer')
 
-
-<script src="{{ asset('assets/js/dist/jquery.min.js') }}"></script>
-<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/js/prism.js') }}"></script>
+<?php if (App::environment() == 'development'): ?>
+    <script src="{{ asset('assets/js/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/prism.js') }}"></script>
+<?php else: ?>
+    <script src="{{ asset('assets/js/dist/all.min.js') }}"></script>
+<?php endif; ?>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
