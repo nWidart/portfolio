2bda7d5174ba0222a32aefa38674f4c4f5a19c4c
Nicolas Widart <n.widart@gmail.com>
2014-09-21 10:47:28 +0200

    Add the blog container and h1 title.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,12 +5,17 @@ Blog | @parent
 @stop
 
 @section('content')
-<ul>
-    <?php foreach($posts as $post): ?>
-        <li>
-            <span class="date">{{ $post->date->format('d-m-Y') }}</span>
-            <h3><a href="{{ URL::route('blog.show', [$post->slug]) }}"><?php echo $post->status == 'draft' ? '[Draft]' : ''; ?> {{ $post->title }}</a></h3>
-        </li>
-    <?php endforeach; ?>
-</ul>
+<div class="row">
+    <div class="col-md-12">
+        <h1>Blog</h1>
+        <ul>
+           <?php foreach($posts as $post): ?>
+               <li>
+                   <span class="date">{{ $post->date->format('d-m-Y') }}</span>
+                   <h3><a href="{{ URL::route('blog.show', [$post->slug]) }}"><?php echo $post->status == 'draft' ? '[Draft]' : ''; ?> {{ $post->title }}</a></h3>
+               </li>
+           <?php endforeach; ?>
+        </ul>
+    </div>
+</div>
 @stop
