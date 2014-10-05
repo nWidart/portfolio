a3d278f0282181e242f7f6b3c40781c55c96c346
Nicolas Widart <n.widart@gmail.com>
2014-10-05 20:36:54 +0200

    Add the GollumEvent

---
+++ app/Services/Activity/Github/Events/GollumEvent.php
@@ -0,0 +1,16 @@
+<?php namespace Nwidart\Services\Activity\Github\Events;
+
+class GollumEvent extends BaseEventClass implements GithubEventInterface
+{
+    public function handle($eventData)
+    {
+        return [
+            'time' => $this->getDate($eventData['created_at']),
+            'actor' => $eventData['actor']['login'],
+            'actor_avatar' => $eventData['actor']['avatar_url'],
+            'verb' => "Updated Wiki ",
+            'action_object' => $eventData['payload']['pages'][0]['page_name'],
+            'target' => $eventData['payload']['pages'][0]['html_url']
+        ];
+    }
+}
