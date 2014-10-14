825cdb15f4bd3be7dcdabe2752e3401191057942
Nicolas Widart <n.widart@gmail.com>
2014-10-14 09:34:35 +0200

    Caching the book data.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,17 +2,28 @@
 
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
     public function index(Filesystem $finder)
     {
-        $books = json_decode($finder->get(base_path() . '/data/books/books.json'));
+        $books = $this->getBooksData($finder);
 
         $reading = $books->reading;
         $read = $books->read;
         $toRead = $books->toRead;
 
         return view('pages.book-library', compact('reading', 'read', 'toRead'));
+    }
+
+    private function getBooksData(Filesystem $finder)
+    {
+        if (!Cache::has('books')) {
+            $books = json_decode($finder->get(base_path() . '/data/books/books.json'));
+            Cache::put('books', $books, 1440);
+        }
+
+        return Cache::get('books', []);
     }
 }
