56f2690e3ae0d5a986b872b37b1831befb4f59c6
Nicolas Widart <n.widart@gmail.com>
2016-01-11 13:48:02 +0100

    Binding the latest posts to homepage

--- Modules/Blog/composers.php
+++ Modules/Blog/composers.php
@@ -1,3 +1,3 @@
 <?php
 
-view()->composer('blog.*', 'Modules\Blog\Composers\Frontend\LatestPostsComposer');
+view()->composer(['blog.*', 'home'], 'Modules\Blog\Composers\Frontend\LatestPostsComposer');
