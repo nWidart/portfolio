6f0208b7e06f6618d1dbf1208cc5fa84156d2e70
Nicolas Widart <n.widart@gmail.com>
2016-01-11 17:25:59 +0100

    Ordering on date

--- Modules/Book/Resources/views/admin/books/index.blade.php
+++ Modules/Book/Resources/views/admin/books/index.blade.php
@@ -118,7 +118,7 @@
                 "sort": true,
                 "info": true,
                 "autoWidth": true,
-                "order": [[ 0, "desc" ]],
+                "order": [[ 3, "desc" ]],
                 "language": {
                     "url": '<?php echo Module::asset("core:js/vendor/datatables/{$locale}.json") ?>'
                 }
