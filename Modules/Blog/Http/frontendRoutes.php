a724b232d1475a7f4e43ea1a0e0f08dfe342aabf
Nicolas Widart <n.widart@gmail.com>
2016-01-11 13:47:53 +0100

    Renaming route

--- Modules/Blog/Http/frontendRoutes.php
+++ Modules/Blog/Http/frontendRoutes.php
@@ -7,5 +7,5 @@ use Illuminate\Routing\Router;
 $router->group(['prefix' => 'blog'], function (Router $router) {
     $locale = LaravelLocalization::setLocale() ?: App::getLocale();
     $router->get('/', ['as' => $locale . '.blog', 'uses' => 'PublicController@index']);
-    $router->get('{slug}', ['as' => $locale . '.blog.slug', 'uses' => 'PublicController@show']);
+    $router->get('{slug}', ['as' => $locale . '.blog.show', 'uses' => 'PublicController@show']);
 });
