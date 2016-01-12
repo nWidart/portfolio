44a833d074ff91f6dbc5b741dca74ffdb11e46ca
Nicolas Widart <n.widart@gmail.com>
2016-01-12 13:56:02 +0100

    Adding the contact ways on separate column

--- Themes/FlatlyNw/views/contact.blade.php
+++ Themes/FlatlyNw/views/contact.blade.php
@@ -10,20 +10,27 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-8">
             <h1>Contact me</h1>
             {!! $page->body !!}
+        </div>
+        <div class="col-md-4">
+            <h2>Contact platforms</h2>
             <ul class="bullet">
-                <li>Email: <a href="mailto:n.widart@gmail.com">n.widart@gmail.com</a></li>
-                <li>Skype: <a href="skype:live:n.widart">live:n.widart</a></li>
                 <li>
-                    <a href="https://github.com/nWidart" target="_blank">Github</a>
+                    <a href="mailto:n.widart@gmail.com"><i class="fa fa-envelope"></i> n.widart@gmail.com</a>
                 </li>
                 <li>
-                    <a href="http://be.linkedin.com/pub/nicolas-widart/25/7ab/a63" target="_blank">LinkedIn</a>
+                    <a href="skype:live:n.widart"><i class="fa fa-skype"></i> live:n.widart</a>
                 </li>
                 <li>
-                    <a href="http://twitter.com/nicolaswidart" target="_blank">Twitter</a>
+                    <a href="https://github.com/nWidart" target="_blank"><i class="fa fa-github"></i> Github</a>
+                </li>
+                <li>
+                    <a href="http://be.linkedin.com/pub/nicolas-widart/25/7ab/a63" target="_blank"><i class="fa fa-linkedin"></i> LinkedIn</a>
+                </li>
+                <li>
+                    <a href="http://twitter.com/nicolaswidart" target="_blank"><i class="fa fa-twitter"></i> Twitter</a>
                 </li>
             </ul>
         </div>
