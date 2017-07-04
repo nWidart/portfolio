c8d1cd7f7fec11b1b36207e79551b9342a050465
Nicolas Widart <n.widart@gmail.com>
2017-07-04 13:00:22 +0200

    Create an empty member event as a quick fix

---
+++ Modules/Activity/Services/Activity/Github/Events/MemberEvent.php
@@ -0,0 +1,9 @@
+<?php namespace Modules\Activity\Services\Activity\Github\Events;
+
+class MemberEvent implements GithubEventInterface
+{
+    public function handle($eventData)
+    {
+        return [];
+    }
+}
